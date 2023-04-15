1450b8faf837efef5561b50bb5663c9bfab34ead
barryb <jiayi.li2@my.jcu.edu.au>
2023-04-15 19:29:40 +0800

    add prac 5 files

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,51 +1,52 @@
-import csv
-
-def read_csv_file(filename):
-    """Reads the CSV file and returns a list of lists."""
-    data = []
-    with open(filename, "r", encoding="utf-8-sig") as in_file:
-        reader = csv.reader(in_file)
-        for row in reader:
-            data.append(row)
-    return data
-
-def get_champions(data):
-    """Returns a dictionary of champions and how many times they have won."""
-    champions = {}
-    for row in data:
-        if row[0] != 'Year':
-            name = row[1]
-            if name in champions:
-                champions[name] += 1
-            else:
-                champions[name] = 1
-    return champions
-
-def get_countries(data):
-    """Returns a set of countries of the champions in alphabetical order."""
-    countries = set()
-    for row in data:
-        if row[0] != 'Year':
-            country = row[2]
-            countries.add(country)
-    return sorted(countries)
-
-def display_results(champions, countries):
-    """Displays the processed information."""
-    print("Champions:")
-    for name, count in champions.items():
-        print(f"{name}: {count}")
-    print("Countries:")
-    countries_str = ", ".join(countries)
-    print(countries_str)
+"""
+ estimate: 40 mins
+ actual: 60 mins
+ """
 
 def main():
-    filename = "wimbledon.csv"
-    data = read_csv_file(filename)
-    champions = get_champions(data)
-    countries = get_countries(data)
-    display_results(champions, countries)
+     filename = "wimbledon.csv"
+     data = read_csv_file(filename)
+     champions = get_champions(data)
+     countries = get_countries(data)
+     print_champions(champions)
+     print()
+     print_countries(countries)
 
-if __name__ == "__main__":
-    main()
+ def read_csv_file(filename):
+     with open(filename, "r", encoding="utf-8-sig") as in_file:
+         # start reading from the second line
+         data = in_file.readlines()[1:]
+         # split each line by commas and store in a list
+         data = [line.strip().split(",") for line in data]
+         return data
+
+ def get_champions(data):
+     champions = {}
+     for line in data:
+         winner = line[2]
+         if winner in champions:
+             champions[winner] += 1
+         else:
+             champions[winner] = 1
+     return champions
+
+
+ def get_countries(data):
+     countries = set()
+     for line in data:
+         country = line[1]
+         countries.add(country)
+     return sorted(countries)
+
+ def print_champions(champions):
+     print("Wimbledon Champions:")
+     for champion, count in champions.items():
+         print(f"{champion} {count}")
+
+ def print_countries(countries):
+     print(f"These {len(countries)} countries have won Wimbledon:")
+     print(", ".join(countries))
+
+
+ main()
 
