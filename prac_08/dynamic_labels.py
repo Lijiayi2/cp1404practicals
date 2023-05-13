223781ca044d886bcc5474dc9fc796a7b47a3b8d
barryb <jiayi.li2@my.jcu.edu.au>
2023-05-13 15:48:43 +0800

    add prac 8 files

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -15,6 +15,16 @@ class DynamicLabelsApp(App):
             layout.add_widget(label)
         return layout
 
+    def create_labels(self):
+        """Create labels from data and add them to the GUI."""
+        for name in self.name_list:
+            label = Label(text=name)
+            self.root.ids.main_layout.add_widget(label)
+
+    def clear_all_labels(self):
+        """Clear all labels"""
+        self.root.ids.main_layout.clear_widgets()
+
 
 if __name__ == '__main__':
     DynamicLabelsApp().run()
